bacb157c6ad2287d0826a7ea067e821a8126ba0b
ooo1324 <ooo1324@daum.net>
2024-03-28 01:52:29 +0900

    Chore : .gitignore, Add Character ,Game,Player Code

---
+++ WarKing/Source/WarKing/Character/WKCharacterPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/WKCharacterBase.h"
+#include "WKCharacterPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARKING_API AWKCharacterPlayer : public AWKCharacterBase
+{
+	GENERATED_BODY()
+	
+};
--- WarKing/Source/WarKing/WarKing.Build.cs
+++ WarKing/Source/WarKing/WarKing.Build.cs
@@ -7,8 +7,10 @@ public class WarKing : ModuleRules
 	public WarKing(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+        PublicIncludePaths.AddRange(new string[] { "WarKing" });
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
