772c0e0ecc8c95899e0bf53a51747c15d8c1997d
ooo1324 <ooo1324@daum.net>
2024-05-09 18:50:21 +0900

    Feat : HUD Ghost Progress Bar

---
+++ WarKing/Source/WarKing/GA/WKGA_Skill_FlamingSword.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GA/WKGA_Skill_FlamingSword.h"
+#include "Character/WKCharacterPlayer.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "WarKing.h"
+
+UWKGA_Skill_FlamingSword::UWKGA_Skill_FlamingSword()
+{
+}
+
+void UWKGA_Skill_FlamingSword::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	AWKCharacterPlayer* TargetCharacter = Cast<AWKCharacterPlayer>(ActorInfo->AvatarActor.Get());
+	if (!TargetCharacter)
+	{
+		return;
+	}
+
+	if (!ActiveSkillActionMontage)
+	{
+		return;
+	}
+
+	TargetCharacter->GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
+
+	UAbilityTask_PlayMontageAndWait* PlayMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, TEXT("SkillMontage"), ActiveSkillActionMontage, 1.0f);
+	PlayMontageTask->OnCompleted.AddDynamic(this, &UWKGA_Skill_FlamingSword::OnCompleteCallback);
+	PlayMontageTask->OnInterrupted.AddDynamic(this, &UWKGA_Skill_FlamingSword::OnInterruptedCallback);
+
+	PlayMontageTask->ReadyForActivation();
+}
+
+void UWKGA_Skill_FlamingSword::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	AWKCharacterPlayer* TargetCharacter = Cast<AWKCharacterPlayer>(ActorInfo->AvatarActor.Get());
+	if (TargetCharacter)
+	{
+		TargetCharacter->GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Walking);
+	}
+
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UWKGA_Skill_FlamingSword::OnCompleteCallback()
+{
+	bool bReplicatedEndAbility = true;
+	bool bWasCancelled = false;
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicatedEndAbility, bWasCancelled);
+}
+
+void UWKGA_Skill_FlamingSword::OnInterruptedCallback()
+{
+	bool bReplicatedEndAbility = true;
+	bool bWasCancelled = true;
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicatedEndAbility, bWasCancelled);
+}
---
+++ WarKing/Source/WarKing/GA/WKGA_Skill_FlamingSword.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GA/WKGameplayAbility.h"
+#include "WKGA_Skill_FlamingSword.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARKING_API UWKGA_Skill_FlamingSword : public UWKGameplayAbility
+{
+	GENERATED_BODY()
+
+	public:
+		UWKGA_Skill_FlamingSword();
+
+public:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+protected:
+	UFUNCTION()
+	void OnCompleteCallback();
+
+	UFUNCTION()
+	void OnInterruptedCallback();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animation)
+	TObjectPtr<class UAnimMontage> ActiveSkillActionMontage;
+};
